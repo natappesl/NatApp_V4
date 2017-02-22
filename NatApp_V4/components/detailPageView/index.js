93ec54671cd5a063e94187d42a129707d98a71f0
Naturalist App <engsl@ucmerced.edu>
2017-02-22 19:06:14 +0000

    Changes made by Naturalist App (engsl@ucmerced.edu).

---
+++ NatApp_V4/components/detailPageView/index.js
@@ -0,0 +1,269 @@
+'use strict';
+
+app.detailPageView = kendo.observable({
+    onShow: function() {},
+    afterShow: function() {}
+});
+app.localization.registerView('detailPageView');
+
+// START_CUSTOM_CODE_detailPageView
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_detailPageView
+(function(parent) {
+    var dataProvider = app.data.backendServices,
+        /// start global model properties
+        /// end global model properties
+        fetchFilteredData = function(paramFilter, searchFilter) {
+            var model = parent.get('detailPageViewModel'),
+                dataSource;
+
+            if (model) {
+                dataSource = model.get('dataSource');
+            } else {
+                parent.set('detailPageViewModel_delayedFetch', paramFilter || null);
+                return;
+            }
+
+            if (paramFilter) {
+                model.set('paramFilter', paramFilter);
+            } else {
+                model.set('paramFilter', undefined);
+            }
+
+            if (paramFilter && searchFilter) {
+                dataSource.filter({
+                    logic: 'and',
+                    filters: [paramFilter, searchFilter]
+                });
+            } else if (paramFilter || searchFilter) {
+                dataSource.filter(paramFilter || searchFilter);
+            } else {
+                dataSource.filter({});
+            }
+        },
+
+        flattenLocationProperties = function(dataItem) {
+            var propName, propValue,
+                isLocation = function(value) {
+                    return propValue && typeof propValue === 'object' &&
+                        propValue.longitude && propValue.latitude;
+                };
+
+            for (propName in dataItem) {
+                if (dataItem.hasOwnProperty(propName)) {
+                    propValue = dataItem[propName];
+                    if (isLocation(propValue)) {
+                        dataItem[propName] =
+                            kendo.format('Latitude: {0}, Longitude: {1}',
+                                propValue.latitude, propValue.longitude);
+                    }
+                }
+            }
+        },
+        dataSourceOptions = {
+            type: 'everlive',
+            transport: {
+                typeName: 'Species',
+                dataProvider: dataProvider
+            },
+            group: {
+                field: 'Type'
+            },
+            change: function(e) {
+                var data = this.data();
+                for (var i = 0; i < data.length; i++) {
+                    var dataItem = data[i];
+
+                    /// start flattenLocation property
+                    flattenLocationProperties(dataItem);
+                    /// end flattenLocation property
+
+                }
+            },
+            error: function(e) {
+
+                if (e.xhr) {
+                    var errorText = "";
+                    try {
+                        errorText = JSON.stringify(e.xhr);
+                    } catch (jsonErr) {
+                        errorText = e.xhr.responseText || e.xhr.statusText || 'An error has occurred!';
+                    }
+                    alert(errorText);
+                }
+            },
+            schema: {
+                model: {
+                    fields: {
+                        'Name': {
+                            field: 'Name',
+                            defaultValue: ''
+                        },
+                    }
+                }
+            },
+            serverFiltering: true,
+            serverSorting: true,
+            sort: {
+                field: 'Name',
+                dir: 'asc'
+            },
+        },
+        /// start data sources
+        /// end data sources
+        detailPageViewModel = kendo.observable({
+            _dataSourceOptions: dataSourceOptions,
+            searchChange: function(e) {
+                var searchVal = e.target.value,
+                    searchFilter;
+
+                if (searchVal) {
+                    searchFilter = {
+                        field: 'Tags',
+                        operator: 'contains',
+                        value: searchVal
+                    };
+                }
+                fetchFilteredData(detailPageViewModel.get('paramFilter'), searchFilter);
+            },
+            fixHierarchicalData: function(data) {
+                var result = {},
+                    layout = {};
+
+                $.extend(true, result, data);
+
+                (function removeNulls(obj) {
+                    var i, name,
+                        names = Object.getOwnPropertyNames(obj);
+
+                    for (i = 0; i < names.length; i++) {
+                        name = names[i];
+
+                        if (obj[name] === null) {
+                            delete obj[name];
+                        } else if ($.type(obj[name]) === 'object') {
+                            removeNulls(obj[name]);
+                        }
+                    }
+                })(result);
+
+                (function fix(source, layout) {
+                    var i, j, name, srcObj, ltObj, type,
+                        names = Object.getOwnPropertyNames(layout);
+
+                    if ($.type(source) !== 'object') {
+                        return;
+                    }
+
+                    for (i = 0; i < names.length; i++) {
+                        name = names[i];
+                        srcObj = source[name];
+                        ltObj = layout[name];
+                        type = $.type(srcObj);
+
+                        if (type === 'undefined' || type === 'null') {
+                            source[name] = ltObj;
+                        } else {
+                            if (srcObj.length > 0) {
+                                for (j = 0; j < srcObj.length; j++) {
+                                    fix(srcObj[j], ltObj[0]);
+                                }
+                            } else {
+                                fix(srcObj, ltObj);
+                            }
+                        }
+                    }
+                })(result, layout);
+
+                return result;
+            },
+            itemClick: function(e) {
+                var dataItem = e.dataItem || detailPageViewModel.originalItem;
+
+                app.mobileApp.navigate('#components/detailPageView/details.html?uid=' + dataItem.uid);
+
+            },
+            detailsShow: function(e) {
+                var uid = e.view.params.uid,
+                    dataSource = detailPageViewModel.get('dataSource'),
+                    itemModel = dataSource.getByUid(uid);
+
+                detailPageViewModel.setCurrentItemByUid(uid);
+
+                /// start detail form show
+                /// end detail form show
+            },
+            setCurrentItemByUid: function(uid) {
+                var item = uid,
+                    dataSource = detailPageViewModel.get('dataSource'),
+                    itemModel = dataSource.getByUid(item);
+                itemModel.ImageUrl = processImage(itemModel.Image);
+
+                if (!itemModel.Name) {
+                    itemModel.Name = String.fromCharCode(160);
+                }
+
+                /// start detail form initialization
+                /// end detail form initialization
+
+                detailPageViewModel.set('originalItem', itemModel);
+                detailPageViewModel.set('currentItem',
+                    detailPageViewModel.fixHierarchicalData(itemModel));
+
+                return itemModel;
+            },
+            linkBind: function(linkString) {
+                var linkChunks = linkString.split('|');
+                if (linkChunks[0].length === 0) {
+                    return this.get('currentItem.' + linkChunks[1]);
+                }
+                return linkChunks[0] + this.get('currentItem.' + linkChunks[1]);
+            },
+            /// start masterDetails view model functions
+            /// end masterDetails view model functions
+            currentItem: {}
+        });
+
+    if (typeof dataProvider.sbProviderReady === 'function') {
+        dataProvider.sbProviderReady(function dl_sbProviderReady() {
+            parent.set('detailPageViewModel', detailPageViewModel);
+            var param = parent.get('detailPageViewModel_delayedFetch');
+            if (typeof param !== 'undefined') {
+                parent.set('detailPageViewModel_delayedFetch', undefined);
+                fetchFilteredData(param);
+            }
+        });
+    } else {
+        parent.set('detailPageViewModel', detailPageViewModel);
+    }
+
+    parent.set('onShow', function(e) {
+        var param = e.view.params.filter ? JSON.parse(e.view.params.filter) : null,
+            isListmenu = false,
+            backbutton = e.view.element && e.view.element.find('header [data-role="navbar"] .backButtonWrapper'),
+            dataSourceOptions = detailPageViewModel.get('_dataSourceOptions'),
+            dataSource;
+
+        if (param || isListmenu) {
+            backbutton.show();
+            backbutton.css('visibility', 'visible');
+        } else {
+            if (e.view.element.find('header [data-role="navbar"] [data-role="button"]').length) {
+                backbutton.hide();
+            } else {
+                backbutton.css('visibility', 'hidden');
+            }
+        }
+
+        dataSource = new kendo.data.DataSource(dataSourceOptions);
+        detailPageViewModel.set('dataSource', dataSource);
+        fetchFilteredData(param);
+    });
+
+})(app.detailPageView);
+
+// START_CUSTOM_CODE_detailPageViewModel
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_detailPageViewModel
---
+++ NatApp_V4/components/homeView/index.js
@@ -0,0 +1,256 @@
+'use strict';
+
+app.homeView = kendo.observable({
+    onShow: function() {},
+    afterShow: function() {}
+});
+app.localization.registerView('homeView');
+
+// START_CUSTOM_CODE_homeView
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_homeView
+(function(parent) {
+    var dataProvider = app.data.backendServices,
+        /// start global model properties
+        /// end global model properties
+        fetchFilteredData = function(paramFilter, searchFilter) {
+            var model = parent.get('homeViewModel'),
+                dataSource;
+
+            if (model) {
+                dataSource = model.get('dataSource');
+            } else {
+                parent.set('homeViewModel_delayedFetch', paramFilter || null);
+                return;
+            }
+
+            if (paramFilter) {
+                model.set('paramFilter', paramFilter);
+            } else {
+                model.set('paramFilter', undefined);
+            }
+
+            if (paramFilter && searchFilter) {
+                dataSource.filter({
+                    logic: 'and',
+                    filters: [paramFilter, searchFilter]
+                });
+            } else if (paramFilter || searchFilter) {
+                dataSource.filter(paramFilter || searchFilter);
+            } else {
+                dataSource.filter({});
+            }
+        },
+
+        flattenLocationProperties = function(dataItem) {
+            var propName, propValue,
+                isLocation = function(value) {
+                    return propValue && typeof propValue === 'object' &&
+                        propValue.longitude && propValue.latitude;
+                };
+
+            for (propName in dataItem) {
+                if (dataItem.hasOwnProperty(propName)) {
+                    propValue = dataItem[propName];
+                    if (isLocation(propValue)) {
+                        dataItem[propName] =
+                            kendo.format('Latitude: {0}, Longitude: {1}',
+                                propValue.latitude, propValue.longitude);
+                    }
+                }
+            }
+        },
+        dataSourceOptions = {
+            type: 'everlive',
+            transport: {
+                typeName: 'Species',
+                dataProvider: dataProvider
+            },
+            group: {
+                field: 'Type'
+            },
+            change: function(e) {
+                var data = this.data();
+                for (var i = 0; i < data.length; i++) {
+                    var dataItem = data[i];
+
+                    /// start flattenLocation property
+                    flattenLocationProperties(dataItem);
+                    /// end flattenLocation property
+
+                }
+            },
+            error: function(e) {
+
+                if (e.xhr) {
+                    var errorText = "";
+                    try {
+                        errorText = JSON.stringify(e.xhr);
+                    } catch (jsonErr) {
+                        errorText = e.xhr.responseText || e.xhr.statusText || 'An error has occurred!';
+                    }
+                    alert(errorText);
+                }
+            },
+            schema: {
+                model: {
+                    fields: {
+                        'Name': {
+                            field: 'Name',
+                            defaultValue: ''
+                        },
+                    }
+                }
+            },
+            serverFiltering: true,
+            serverSorting: true,
+            sort: {
+                field: 'Name',
+                dir: 'asc'
+            },
+        },
+        /// start data sources
+        /// end data sources
+        homeViewModel = kendo.observable({
+            _dataSourceOptions: dataSourceOptions,
+            fixHierarchicalData: function(data) {
+                var result = {},
+                    layout = {};
+
+                $.extend(true, result, data);
+
+                (function removeNulls(obj) {
+                    var i, name,
+                        names = Object.getOwnPropertyNames(obj);
+
+                    for (i = 0; i < names.length; i++) {
+                        name = names[i];
+
+                        if (obj[name] === null) {
+                            delete obj[name];
+                        } else if ($.type(obj[name]) === 'object') {
+                            removeNulls(obj[name]);
+                        }
+                    }
+                })(result);
+
+                (function fix(source, layout) {
+                    var i, j, name, srcObj, ltObj, type,
+                        names = Object.getOwnPropertyNames(layout);
+
+                    if ($.type(source) !== 'object') {
+                        return;
+                    }
+
+                    for (i = 0; i < names.length; i++) {
+                        name = names[i];
+                        srcObj = source[name];
+                        ltObj = layout[name];
+                        type = $.type(srcObj);
+
+                        if (type === 'undefined' || type === 'null') {
+                            source[name] = ltObj;
+                        } else {
+                            if (srcObj.length > 0) {
+                                for (j = 0; j < srcObj.length; j++) {
+                                    fix(srcObj[j], ltObj[0]);
+                                }
+                            } else {
+                                fix(srcObj, ltObj);
+                            }
+                        }
+                    }
+                })(result, layout);
+
+                return result;
+            },
+            itemClick: function(e) {
+                var dataItem = e.dataItem || homeViewModel.originalItem;
+
+                app.mobileApp.navigate('#components/homeView/details.html?uid=' + dataItem.uid);
+
+            },
+            detailsShow: function(e) {
+                var uid = e.view.params.uid,
+                    dataSource = homeViewModel.get('dataSource'),
+                    itemModel = dataSource.getByUid(uid);
+
+                homeViewModel.setCurrentItemByUid(uid);
+
+                /// start detail form show
+                /// end detail form show
+            },
+            setCurrentItemByUid: function(uid) {
+                var item = uid,
+                    dataSource = homeViewModel.get('dataSource'),
+                    itemModel = dataSource.getByUid(item);
+                itemModel.ImageUrl = processImage(itemModel.Image);
+
+                if (!itemModel.Type) {
+                    itemModel.Type = String.fromCharCode(160);
+                }
+
+                /// start detail form initialization
+                /// end detail form initialization
+
+                homeViewModel.set('originalItem', itemModel);
+                homeViewModel.set('currentItem',
+                    homeViewModel.fixHierarchicalData(itemModel));
+
+                return itemModel;
+            },
+            linkBind: function(linkString) {
+                var linkChunks = linkString.split('|');
+                if (linkChunks[0].length === 0) {
+                    return this.get('currentItem.' + linkChunks[1]);
+                }
+                return linkChunks[0] + this.get('currentItem.' + linkChunks[1]);
+            },
+            /// start masterDetails view model functions
+            /// end masterDetails view model functions
+            currentItem: {}
+        });
+
+    if (typeof dataProvider.sbProviderReady === 'function') {
+        dataProvider.sbProviderReady(function dl_sbProviderReady() {
+            parent.set('homeViewModel', homeViewModel);
+            var param = parent.get('homeViewModel_delayedFetch');
+            if (typeof param !== 'undefined') {
+                parent.set('homeViewModel_delayedFetch', undefined);
+                fetchFilteredData(param);
+            }
+        });
+    } else {
+        parent.set('homeViewModel', homeViewModel);
+    }
+
+    parent.set('onShow', function(e) {
+        var param = e.view.params.filter ? JSON.parse(e.view.params.filter) : null,
+            isListmenu = false,
+            backbutton = e.view.element && e.view.element.find('header [data-role="navbar"] .backButtonWrapper'),
+            dataSourceOptions = homeViewModel.get('_dataSourceOptions'),
+            dataSource;
+
+        if (param || isListmenu) {
+            backbutton.show();
+            backbutton.css('visibility', 'visible');
+        } else {
+            if (e.view.element.find('header [data-role="navbar"] [data-role="button"]').length) {
+                backbutton.hide();
+            } else {
+                backbutton.css('visibility', 'hidden');
+            }
+        }
+
+        dataSource = new kendo.data.DataSource(dataSourceOptions);
+        homeViewModel.set('dataSource', dataSource);
+        fetchFilteredData(param);
+    });
+
+})(app.homeView);
+
+// START_CUSTOM_CODE_homeViewModel
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_homeViewModel
